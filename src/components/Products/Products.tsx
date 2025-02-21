d7c332e2c376bac58c460267c40b234b283fde93
th-neri <nerithiago123@gmail.com>
2025-02-21 16:49:22 -0600

    changed produts and github

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -2,10 +2,10 @@ function Products() {
     return (
         <div className="">
             <div className="flex gap-8 items-center justify-center pb-8">
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-400">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[200px]" src="./src/assets/furniture-1.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[200px]" src="./src/assets/furniture-1.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Syltherine</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Stylish cafe chair</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$141</span>
@@ -15,10 +15,10 @@ function Products() {
                     </a>
                 </div>
 
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 pb-12">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-2.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-2.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Leviosa</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Stylish cafe chair</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$141</span>
@@ -26,10 +26,10 @@ function Products() {
                     </a>
                 </div>
 
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-3.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-3.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Lolito</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Luxury big sofa</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$429</span>
@@ -39,10 +39,10 @@ function Products() {
                     </a>
                 </div>
 
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-4.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-4.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Respira</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Outdoor bar table and stool</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$30</span>
@@ -53,10 +53,10 @@ function Products() {
             </div>
 
             <div className="flex gap-8 items-center justify-center pb-8">
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 pb-12">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-5.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-5.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Grifo</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Night lamp</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$91</span>
@@ -64,10 +64,10 @@ function Products() {
                     </a>
                 </div>
 
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-6.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-6.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Muggo</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Small couch</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$220</span>
@@ -76,10 +76,10 @@ function Products() {
                     </a>
                 </div>
 
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-7.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-7.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Pingky</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Cute bed set</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$429</span>
@@ -89,10 +89,10 @@ function Products() {
                     </a>
                 </div>
 
-                <div className="bg-p w-[285px] h-[446x] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
+                <div className="inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300">
                     <a href="#">
-                        <img className="min-w-[300px] min-h-[300px] " src="./src/assets/furniture-8.png" alt="" />
-                        <div className="flex-col pt-4 pr-5 pb-8 pl-4">
+                        <img className="min-w-[285px] min-h-[300px] " src="./src/assets/furniture-8.png" alt="" />
+                        <div className="bg-p w-[285px] h-[145px] flex-col pt-4 pr-5 pb-8 pl-4">
                             <h1 className="font-poppins font-semibold text-2xl text-black/80 pb-2">Potty</h1>
                             <h1 className="font-poppins font-medium text-base text-black/50 pb-2">Flower pot</h1>
                             <span className="font-poppins font-semibold text-xl text-black/80">$20</span>
